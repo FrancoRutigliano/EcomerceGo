2be81a16f1f84501836d4bd24a278241565b90a3
FrancoRutigliano <francorutigliano11@gmail.com>
2023-11-17 16:07:30 -0300

    Intanciando las routes de la aplicación

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/FrancoRutigliano/EcommercerGolang/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("/users/signup", controllers.Signup())
+	incomingRoutes.POST("/users/login", controllers.Login())
+	incomingRoutes.POST("admin/addproduct", controllers.ProductViewAdmin())
+	incomingRoutes.GET("/users/productview", controllers.SearchProduct())
+	incomingRoutes.GET("/users/search", controllers.SearchProductByQuerie())
+}
